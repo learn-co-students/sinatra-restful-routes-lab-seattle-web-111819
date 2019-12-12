94a53e93e1a09c3dc44b01b55fe9afb396c56669
4myc <missch3n@gmail.com>
2019-12-11 18:07:48 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,53 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+  end
+
+  #index page
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  #new page with form to create new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #find each recipe by id && show it in the show page
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  #creates a new recipe and posts it to its own page
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time]) 
+    redirect to "/recipes/#{@recipe.id}"
+  end 
+
+  #load an edit page
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  #edit a recipe page
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  #remember to add delete form to show.erb
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect "/recipes"
+  end
 
 end
